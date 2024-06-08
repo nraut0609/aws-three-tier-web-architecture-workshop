f4f2a4d7217f5ae9c7c520b0e60833a1b13e1136
Nikhil Raut <82317039+nraut0609@users.noreply.github.com>
2024-06-08 16:56:41 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.cluster-c3ca26gewg8y.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'redhat123',
+    DB_DATABASE : 'webappdb'
 });
